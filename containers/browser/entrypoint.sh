583497b0fc3ecf4da0e7c018003005f779662fe9
JASHWANTH SINGH <jashwanthsingh@JASHWANTHs-MacBook-Air.local>
2026-05-24 12:53:22 +0530

    fix: switch browser container to debian to fix chromium snap issue

--- containers/browser/entrypoint.sh
+++ containers/browser/entrypoint.sh
@@ -16,9 +16,9 @@ websockify --web /usr/share/novnc 6082 localhost:5901 &
 # Wait a moment for X to start
 sleep 3
 
-# Launch Chrome
+# Launch Chromium
 export DISPLAY=:1
-google-chrome --no-sandbox --disable-gpu --start-maximized &
+chromium --no-sandbox --disable-gpu --start-maximized &
 
 # Keep container alive
 tail -f /dev/null
